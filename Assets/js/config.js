877403170298d40b6d633462f3599895f6ca15ef
HobbesHub <eaestrada488@gmail.com>
2023-10-14 13:13:52 -0700

    Moved Index.html

---
+++ Assets/js/config.js
@@ -0,0 +1,6 @@
+//This file will be added to .gitignore - which is not working right now with proper env setup 
+//but will still use it to learn about export/import
+
+//apikey for openweathermap API access 
+
+export const apiKey = "cf12e036ffb54077b7c19cba5c2985ac"; 
